7a660fa22506dc18b7f9a870c9bf64f7de188552
semenov_spark <semenov.spark@mail.ru>
2020-02-08 13:45:35 +0300

    validate form

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -8,9 +8,12 @@ formInputs = form.querySelectorAll('.form__input');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    let a = 0;
+
     for (let i = 0; i < formInputs.length; i++) {
         if (!formInputs[i].value) {
             form[i].classList.add('form__input_error');
+            a++;
         } else {
             form[i].classList.remove('form__input_error');
         }
@@ -19,10 +22,18 @@ form.addEventListener('submit', function(event) {
     function validate() {
         let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         let address = inputMail.value;
+        let error;
 
-        if(reg.test(address) == false) {
+        if (reg.test(address) == false) {
             inputMail.classList.add('form__input_error');
-            return false;
+            error = false;
+            return error;
+        } else {
+            error = true;
+        }
+
+        if ((error == true) && (a == 0)) {
+            form.submit();
         }
     }
 
